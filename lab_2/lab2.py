731db47f816a2b327640627dc77bf062698372ef
Nightbot1448 <night1337bot@gmail.com>
2019-03-03 20:12:17 +0300

    rename variables

--- lab_2/lab2.py
+++ lab_2/lab2.py
@@ -7,6 +7,9 @@ width = 640
 height = 480
 
 def scissor():
+    x_start, y_start = 100, 100
+    width, height = 200, 200
+
     glClearColor(255, 255, 255, 1)
     x_scale = 1/16*width
     y_scale = 1/16*width
@@ -14,8 +17,6 @@ def scissor():
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE) 
     glLineWidth(3)
 
-    x_start, y_start = 100, 100
-    width, height = 200, 200
     glEnable(GL_SCISSOR_TEST)
     glScissor(x_start, y_start, width, height)
     glBegin(GL_TRIANGLE_STRIP)
@@ -61,6 +62,8 @@ def scissor():
 
 
 def alpha():
+    func = GL_GEQUAL
+    ref = 0.5
     glClearColor(255, 255, 255, 1)
     # x_scale = 1/7*glutGet(GLUT_WINDOW_WIDTH)
     # y_scale = 1/8*glutGet(GLUT_WINDOW_HEIGHT)
@@ -69,7 +72,7 @@ def alpha():
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL) 
     glEnable(GL_ALPHA_TEST)
 
-    glAlphaFunc(GL_GEQUAL, 0.5)
+    glAlphaFunc(func, ref)
 
     glBegin(GL_QUAD_STRIP)
     # glColor3d(255, 255, 0)
