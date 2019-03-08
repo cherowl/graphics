5647ef5e8ba4358d991effb8a05c8f3eae62045b
Nightbot1448 <night1337bot@gmail.com>
2019-03-09 01:01:50 +0300

    simple extension

--- lab_3/lab3.py
+++ lab_3/lab3.py
@@ -2,10 +2,12 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 import sys
 import numpy as np
-order = 5 # main variable
+order = 1 # main variable
 
 rotate_matrix = lambda angle: np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
-mirror_matrix = np.array([[-1, 0], [0, 1]])
+h_mirror_matrix = np.array([[-1, 0], [0, 1]])
+v_mirror_matrix = np.array([[1, 0], [0, -1]])
+
 
 def rotate(arr, angle = np.pi/2):
     rot = rotate_matrix(angle)
@@ -20,8 +22,8 @@ def get_objects(basic_triangles, basic_lines, connections, depth):
         triangles -= shift
         lines -= shift
 
-        triangles_copy = np.matmul(triangles.copy(), mirror_matrix)
-        lines_copy = np.matmul(lines.copy(), mirror_matrix)   
+        triangles_copy = np.matmul(triangles.copy(), h_mirror_matrix)
+        lines_copy = np.matmul(lines.copy(), h_mirror_matrix)   
 
         triangles = np.append(triangles, triangles_copy, axis=0)
         lines = np.append(lines, lines_copy, axis=0)
@@ -30,24 +32,43 @@ def get_objects(basic_triangles, basic_lines, connections, depth):
         if depth > 1:
             connections = rotate(connections/2**(0.5), 3*np.pi/4)
             connections -= shift
-            connections_copy = np.matmul(connections.copy(), mirror_matrix)
+            connections_copy = np.matmul(connections.copy(), h_mirror_matrix)
             connections = np.append(connections, connections_copy, axis=0)
         point_index = 2**((depth-1) if (depth > 1) else 0) - 1
         connections = np.vstack((connections, lines[point_index]))
-        connections = np.vstack((connections, np.matmul(connections[-1],mirror_matrix)))
+        connections = np.vstack((connections, np.matmul(connections[-1],h_mirror_matrix)))
         if depth == 1:
             connections = connections[2:]
     
         return get_objects(triangles, lines, connections, depth + 1)
     else:
-        return basic_triangles, np.append(basic_lines, connections, axis=0)
+        basic_lines = np.append(basic_lines, connections, axis=0)
+        shift = np.array([[0, 0.48]])
+        basic_triangles += shift
+        basic_lines += shift
 
+        added = np.array([[0,0]])
+        # print(basic_lines)
+        # print(2**(order-1)-1, 2**(order)-1)
+        added = np.vstack((added, basic_lines[2**(order-1)-1]))
+        added = added[1:]
+        added = np.vstack((added, np.matmul(added[0], v_mirror_matrix)))
+        added = np.vstack((added, basic_lines[2**(order)-1]))        
+        added = np.vstack((added, np.matmul(added[-1], v_mirror_matrix)))
+        # print(added)
+
+        
+        basic_triangles = np.append(basic_triangles, rotate(basic_triangles.copy(), np.pi), axis=0)
+        basic_lines = np.append(basic_lines, rotate(basic_lines.copy(), np.pi), axis=0)
+        basic_lines = np.append(basic_lines, added, axis=0)
+        
+
+        return basic_triangles, basic_lines, added
 
 def draw(basic_triangles, basic_lines):
     triangles = basic_triangles
     lines = basic_lines
-    if order-1:
-        triangles, lines = get_objects(triangles, basic_lines, np.array([[0,0],[0,0]]), 1)
+    triangles, lines, added = get_objects(triangles, basic_lines, np.array([[0,0],[0,0]]), 1)
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE) 
     glBegin(GL_TRIANGLES)
     glColor3f(1.0, 0.0, 0.0)
@@ -66,6 +87,13 @@ def draw(basic_triangles, basic_lines):
         glVertex2f(*lines[index])
         glVertex2f(*lines[index+1])
     glEnd()
+
+    # glPointSize(3)
+    # glBegin(GL_POINTS)
+    #---------------------------------------------
+    # for point in added:
+    #     glVertex2f(*point)
+    # glEnd()
 
 
 def display():
