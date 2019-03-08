74904813eddd6cd894470f9214e7554193e8d346
Nightbot1448 <night1337bot@gmail.com>
2019-03-08 20:20:17 +0300

    lab3_need_iface

--- lab_3/lab3.py
+++ lab_3/lab3.py
@@ -2,7 +2,7 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 import sys
 import numpy as np
-order = 7
+order = 5 # main variable
 
 rotate_matrix = lambda angle: np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
 mirror_matrix = np.array([[-1, 0], [0, 1]])
@@ -11,46 +11,43 @@ def rotate(arr, angle = np.pi/2):
     rot = rotate_matrix(angle)
     return np.matmul(arr, rot)
 
-def get_triangles(basic_triangles, order):
-    if order:
+def get_objects(basic_triangles, basic_lines, connections, depth):
+    if depth != order:
         triangles = rotate(basic_triangles/2**(0.5), 3*np.pi/4)
-        triangles -= triangles[3] - basic_triangles[0]
+        lines = rotate(basic_lines/2**(0.5), 3*np.pi/4)
+
+        shift = triangles[3] - basic_triangles[0]
+        triangles -= shift
+        lines -= shift
+
         triangles_copy = np.matmul(triangles.copy(), mirror_matrix)
+        lines_copy = np.matmul(lines.copy(), mirror_matrix)   
+
         triangles = np.append(triangles, triangles_copy, axis=0)
-        return get_triangles(triangles, order - 1)
-    return basic_triangles
+        lines = np.append(lines, lines_copy, axis=0)
+
+        #add connection between copies
+        if depth > 1:
+            connections = rotate(connections/2**(0.5), 3*np.pi/4)
+            connections -= shift
+            connections_copy = np.matmul(connections.copy(), mirror_matrix)
+            connections = np.append(connections, connections_copy, axis=0)
+        point_index = 2**((depth-1) if (depth > 1) else 0) - 1
+        connections = np.vstack((connections, lines[point_index]))
+        connections = np.vstack((connections, np.matmul(connections[-1],mirror_matrix)))
+        if depth == 1:
+            connections = connections[2:]
+    
+        return get_objects(triangles, lines, connections, depth + 1)
+    else:
+        return basic_triangles, np.append(basic_lines, connections, axis=0)
 
 
-def get_lines(triangles, order):
-    lines = np.array([[0,0]])
-    for index in range(0, triangles.shape[0], 4):
-        shift = (triangles[index+2] - triangles[index+1])/3
-        lines = np.vstack((lines, triangles[index] + (triangles[index+3] - triangles[index])/3 + shift))
-        lines = np.vstack((lines, triangles[index] + (triangles[index+3] - triangles[index])*2/3 +shift))
-    lines = lines[1:]
-    print(lines)
-    # temp_len = lines.shape[0]
-    # half_temp = int(temp_len/2)
-    # lines = np.vstack((lines, lines[int(temp_len/4-1)]))
-    # lines = np.vstack((lines, lines[int(3*temp_len/4-1)]))
-    # for i in range(0, temp_len, 4):
-    #         lines = np.vstack((lines, lines[i]))
-    #         lines = np.vstack((lines, lines[i+2]))
-    # if order % 2:
-    #     pass
-    # else:
-    #     pass
-    #     for i in range(1, half_temp, order*2):
-    #         lines = np.vstack((lines, lines[i]))
-    #         lines = np.vstack((lines, lines[i+4]))
-    #         lines = np.vstack((lines, lines[half_temp+i]))
-    #         lines = np.vstack((lines, lines[half_temp+i+4]))
-    return lines
-
-
-def draw(basic_triangles):
+def draw(basic_triangles, basic_lines):
     triangles = basic_triangles
-    triangles = get_triangles(triangles, order-1)
+    lines = basic_lines
+    if order-1:
+        triangles, lines = get_objects(triangles, basic_lines, np.array([[0,0],[0,0]]), 1)
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE) 
     glBegin(GL_TRIANGLES)
     glColor3f(1.0, 0.0, 0.0)
@@ -64,25 +61,20 @@ def draw(basic_triangles):
         glVertex2f(*(triangles[index + 3]))
     glEnd()
     glColor4f(255,255,255,1.0)
-    lines = get_lines(triangles, order)
     glBegin(GL_LINES)
     for index in range(0, lines.shape[0], 2):
         glVertex2f(*lines[index])
         glVertex2f(*lines[index+1])
     glEnd()
-    glPointSize(3)
-    glBegin(GL_POINTS)
-    glVertex2f(-0.2, -0.16)
-    # glVertex2f(-0.32, 0.08)
-    glEnd()
-    
+
 
 def display():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT)
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
     basic_triangles = np.array([[-0.96, -0.48], [0, -0.48], [0, 0.48], [0.96, -0.48]])
-    draw(basic_triangles)
+    basic_lines = np.array([[-0.32, -0.16], [0.32, -0.16]])
+    draw(basic_triangles, basic_lines)
     glutSwapBuffers()
 
 
