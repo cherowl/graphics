b82259f7acde359052060fdc1867f73be87ca46a
Elena Cherkasova <cherowl@yandex.ru>
2019-03-13 00:19:03 +0300

    interface to lab_2

--- lab_2/design.py
+++ lab_2/design.py
@@ -7,7 +7,7 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from gl_base.controller import GLWidget
+from controller import GLWidget
 
 
 class Ui_MainWindow(object):
